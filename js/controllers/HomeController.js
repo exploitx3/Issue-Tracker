bab2e2d31641e121bf7109c1e960a099332de8e3
exploitx3 <gogo333x@gmail.com>
2016-04-21 14:14:19 +0300

    issuesService and HomeController Updated
    dashboard functions added

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,11 +1,36 @@
 "use strict";
 
 app.controller('HomeController',
-    function ($scope, $rootScope, authService, notifyService, pageSize) {
-        $scope.adsParams = {
+    function ($scope, $rootScope, $q, userService, issuesService, authService, notifyService, ngTableParams, $filter, pageSize) {
+        $scope.homeParams = {
             'startPage': 1,
-            'pageSize': pageSize
+            'pageSize': 2
+        };
+        if(authService.isLoggedIn()){
+            userService.setUserChecks();
+        }
+
+        $scope.affiliatedProjects = userService.getAffiliatedProjects();
+
+        $scope.reloadMyIssues = function (page ,pageSize) {
+            var defer = $q.defer();
+            issuesService.getMyIssuesWithPagination(page, pageSize)
+                .then(
+                    function success(data) {
+                        $scope.homeParams.numItems = data.TotalPages * $scope.homeParams.pageSize;
+                        $scope.issues = data.Issues;
+                        defer.resolve();
+                    },
+                    function error(err) {
+                        notifyService.showError('Cannot load issues' + err);
+                        defer.reject();
+                    }
+                );
+            return defer.promise;
         };
 
+        if(authService.isLoggedIn()){
+            $scope.reloadMyIssues(1, 2);
+        }
     }
 );
--- js/services/issuesService.js
+++ js/services/issuesService.js
@@ -1,7 +1,8 @@
 "use strict";
 
-app.factory('issuesService', function ($http, authService, baseServiceUrl) {
-    function addIssue(formattedIssueData, success, error) {
+app.factory('issuesService', function ($http, $q, authService, baseServiceUrl) {
+    function addIssue(formattedIssueData) {
+        var defer = $q.defer();
         var request = 'Title=' + encodeURIComponent(formattedIssueData.title) +
             '&Description=' + encodeURIComponent(formattedIssueData.description) +
             '&DueDate=' + encodeURIComponent(formattedIssueData.dueDate) +
@@ -12,7 +13,6 @@ app.factory('issuesService', function ($http, authService, baseServiceUrl) {
         for (i = 0; i < len; i += 1) {
             request += encodeURIComponent('Labels[' + i + '].Name') + '=' + encodeURIComponent(formattedIssueData.labels[i]) + (i === len - 1 ? '' : '&');
         }
-        console.log(request);
         $http({
             method: 'POST',
             url: baseServiceUrl + '/issues/',
@@ -21,28 +21,119 @@ app.factory('issuesService', function ($http, authService, baseServiceUrl) {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Authorization': 'Bearer ' + sessionStorage['currentUser-Token']
             }
-        }).success(success).error(error);
+        }).success(function (data) {
+            defer.resolve(data);
+        }).error(function (err) {
+            defer.reject(err);
+        });
+
+        return defer.promise;
     }
 
-    function getIssueById(issueId, success, error) {
+    function addComment(issueId, text) {
+        var defer = $q.defer();
+        var request = 'Text=' + encodeURIComponent(text);
+        $http({
+            method: 'POST',
+            url: baseServiceUrl + '/issues/' + issueId + '/comments',
+            data: request,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Bearer ' + sessionStorage['currentUser-Token']
+            }
+        }).success(function (data) {
+            defer.resolve(data);
+        }).error(function (err) {
+            defer.reject(err);
+        });
+
+        return defer.promise;
+    }
+
+    function getIssueById(issueId) {
+        var defer = $q.defer();
         $http({
             method: 'GET',
             url: baseServiceUrl + '/issues/' + issueId,
             headers: authService.getAuthHeaders()
-        }).success(success).error(error);
+        }).success(function (data) {
+            defer.resolve(data);
+        }).error(function (err) {
+            defer.reject(err);
+        });
+
+        return defer.promise;
     }
 
-    function getIssueCommentsById(issueId, success, error) {
+    function getMyIssues() {
+        var defer = $q.defer();
+        $http({
+            method: 'GET',
+            url: baseServiceUrl + '/issues/me?orderBy=DueDate desc, IssueKey&pageSize=999&pageNumber=1',
+            headers: authService.getAuthHeaders()
+        }).success(function (data) {
+            defer.resolve(data);
+        }).error(function (err) {
+            defer.reject(err);
+        });
+
+        return defer.promise;
+    }
+
+    function getMyIssuesWithPagination(startPage, pageSize) {
+        var defer = $q.defer();
+        $http({
+            method: 'GET',
+            url: baseServiceUrl + '/issues/me?orderBy=DueDate desc, IssueKey&pageSize=' + pageSize + '&pageNumber=' + startPage,
+            headers: authService.getAuthHeaders()
+        }).success(function (data) {
+            defer.resolve(data);
+        }).error(function (err) {
+            defer.reject(err);
+        });
+
+        return defer.promise;
+    }
+
+    function getIssueCommentsById(issueId) {
+        var defer = $q.defer();
         $http({
             method: 'GET',
             url: baseServiceUrl + '/issues/' + issueId + '/comments',
             headers: authService.getAuthHeaders()
-        }).success(success).error(error);
+        }).success(function (data) {
+            defer.resolve(data);
+        }).error(function (err) {
+            defer.reject(err);
+        });
+
+        return defer.promise;
     }
 
     return {
         addIssue: addIssue,
+        addComment: addComment,
+        getMyIssues: getMyIssues,
         getIssueById: getIssueById,
-        getIssueCommentsById: getIssueCommentsById
+        getIssueCommentsById: getIssueCommentsById,
+        getMyIssuesWithPagination: getMyIssuesWithPagination
     }
 });
+
+//app.factory('townsService', function ($resource, baseServiceUrl) {
+//    var townsResource = $resource(baseServiceUrl + '/api/towns');
+//    return {
+//        getTowns: function getTowns(success, error) {
+//            return townsResource.query(success, error);
+//        }
+//    }
+//});
+//
+//app.factory('categoriesService', function ($resource, baseServiceUrl) {
+//    var categoriesResource = $resource(baseServiceUrl + '/api/categories');
+//    return {
+//        getCategories: function getCategories(success, error) {
+//            return categoriesResource.query(success, error);
+//        }
+//    }
+//});
